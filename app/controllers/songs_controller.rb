6856d89fff5b95d88adef3ea4257f803a507b0c0
German Cruz <cruzgerman216@yahoo.com>
2020-07-29 02:59:04 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,7 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-    if @song.valid?
+    if @song
       redirect_to song_path(@song)
     else
       render "new"
