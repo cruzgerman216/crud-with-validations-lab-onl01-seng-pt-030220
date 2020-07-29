6ba1e743e389c1ec02ea922a706adc1cb3fd874c
German Cruz <cruzgerman216@yahoo.com>
2020-07-29 05:21:57 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,4 @@
 class Song < ActiveRecord::Base
    validates :title, presence: true
-   validates :release_year, presence: true, if: :release?
+   validates :release_year, presence: true, if: :released?
 end
