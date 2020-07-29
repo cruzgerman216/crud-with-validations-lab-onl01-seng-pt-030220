35b477eed74f2dafd56bc4a6782de93d261b23b7
German Cruz <cruzgerman216@yahoo.com>
2020-07-29 06:11:16 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,8 @@ class Song < ActiveRecord::Base
    validates :title, presence: true
    validates :release_year, presence: true, if: :released?
 
-   def future 
-     if release_year > date.today.year 
+   def future
+     if release_year > date.today.year
        errs.add(:release_year, "future")
      end
    end
