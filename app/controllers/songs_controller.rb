1b4fc20d7539bb902b7b1adc145212cbdbbc384e
German Cruz <cruzgerman216@yahoo.com>
2020-07-29 02:19:06 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,36 +4,22 @@ class SongsController < ApplicationController
 
   end
   def show
-    @author = Author.find(params[:id])
   end
 
   def new
-    @author = Author.new
   end
 
   def create
-    @author = Author.new(author_params)
-    #  @author = Author.create(author_params)
-    if @author.valid?
-      @author.save
-      redirect_to author_path(@author)
-    else
-      render :new
-    end
 
   end
 
   def update
-    @author = Author.find(params[:id])
 
-    @author.update(author_params)
-
-    redirect_to author_path(@post)
   end
 
   private
 
   def author_params
-    params.permit(:name, :email, :phone_number)
   end
 end
+
